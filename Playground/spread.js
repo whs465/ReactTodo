020f4b73653794a04a4165b7b536d8e29e99bc59
William Hernandez Sierra <whs465@gmail.com>
2016-06-06 22:04:32 +0630

    complete handleAddTodo

---
+++ Playground/spread.js
@@ -0,0 +1,9 @@
+// function add(a, b) {   return a + b; }
+//
+// console.log(add(3, 1));
+//
+// var toAdd = [9, 5]; console.log(add(...toAdd)); var groupA = ['jen', 'juan']; var groupB =
+// ['gus', 'pablo']; var final = [   3, ...groupB ]; console.log(final);
+
+var person = ['andrew', 25];
+var person2 = ['jen', 29];
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -6,8 +6,17 @@ var TestUtils = require('react-addons-test-utils');
 
 var TodoApp = require('TodoApp');
 
-describe('TodoApp',() => {
-  it('should exist',() => {
+describe('TodoApp', () => {
+  it('should exist', () => {
     expect('TodoApp').toExist();
   });
+
+  it('should add todo to the todos state on handleAddTodo', () => {
+    var todoText = 'prueba';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  })
 });

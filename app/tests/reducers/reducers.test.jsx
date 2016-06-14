95c07ffba094a9fc8c57999d0c5eabe09271a06e
William Hernandez Sierra <whs465@gmail.com>
2016-06-14 15:05:17 +0630

    add todos reducers and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -19,8 +19,37 @@ describe('Reducers',() => {
       var action = {
         type: 'TOGGLE_SHOW_COMPLETED'
       };
-      var res = reducers.showCompletedReducer(df(false), df(action));
+      var res = reducers.showCompletedReducer(df(false), df(action) );
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '1',
+        text: 'smtg',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '1'
+      };
+      var res = reducers.todosReducer(df(todos),df(action));
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   });
 });

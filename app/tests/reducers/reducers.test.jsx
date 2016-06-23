e9b78283b750501afdaf000823f2e0ce6e306f8e
William Hernandez Sierra <whs465@gmail.com>
2016-06-23 16:09:36 +0630

    wipe todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -78,6 +78,21 @@ describe('Reducers',() => {
       expect(res.length).toEqual(1);
       expect(res[0]).toEqual(todos[0]);
     });
+
+    it('shoul wipe todos on logout', () => {
+      var todos = [{
+        id: '111',
+        test: 'anyrhing',
+        completed: false,
+        createdAt: 3300
+      }];
+      var action = {
+        type: 'LOGOUT'
+      };
+      var res = reducers.todosReducer(df(todos),df(action));
+      expect(res.length).toEqual(0);
+    });
+
   });
 
   describe('authReducer',() => {
